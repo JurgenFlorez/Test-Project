9c36ff7f7bb7be82c1c89de8890d7961298917bf
JurgenFlorez <jfgel3@gmail.com>
2024-04-23 17:57:46 -0500

    Crear componente Contador

---
+++ src/components/icons/contador.vue
@@ -0,0 +1,27 @@
+<template>
+  <!-- Estructura visual del componente -->
+  <h1>Hello Jurgen</h1>
+  <p>{{ contador }}</p>
+  <button @click="incrementar">Incrementar</button>
+  <button @click="decrementar">Decrementar</button>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+// Definición de variables y funciones
+const contador = ref(0); // Variable reactiva que almacena el contador
+const incrementar = () => {
+  contador.value += 7; // Función para incrementar el contador en 7
+};
+
+const decrementar = () => {
+  contador.value -= 7; // Función para decrementar el contador en 7
+};
+</script>
+
+<style scoped>
+p {
+  color: rgb(89, 208, 226);
+}
+</style>

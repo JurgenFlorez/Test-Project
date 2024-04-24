ae8b05f5fcc609e096617917354983873691a499
JurgenFlorez <jfgel3@gmail.com>
2024-04-24 11:55:48 -0500

    Repositorio 3 con algunos errores de Tyscrip y de setup

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
+  <!-- Definición del componente principal -->
   <contador></contador>
+  <ListaDeTarea></ListaDeTarea>
+  <!-- Se inserta el componente "contador" posicion en la interfaz de usuario -->
 </template>
 
 <script setup>
-import contador from "./components/icons/contador.vue";
+import contador from "./components/icons/contadorFromBotton.vue"; // Se importa el componente "contador"
+import ListaDeTarea from "./components/ListaDeTarea.vue";
 </script>
 
 <style scoped></style>
+<!-- Estilos CSS específicos para este componente -->
---
+++ src/components/ListaDeTarea.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h2>Lista De Tareas</h2>
+    <input type="text" v-model = "nuevaTarea" @keyup.enter="addTarea">
+    <ul>
+      <li v-for= "(tarea, index) in tareas" :key="index">
+               {{ tarea }}
+      </li>
+      
+    </ul>
+  </div>
+</template>
+
+
+<script setup
+
+import { ref } from 'vue';
+// Importa la función ref de Vue REACTIVIDAD
+
+const tareas = ref([]);
+// Crea una referencia reactiva llamada "tareas" con un array vacío como valor inicial
+
+const nuevaTarea = ref('');
+// Crea una referencia reactiva llamada "nuevaTarea" con una cadena vacía como valor inicial
+
+const addTarea = () => {
+  tareas.value.push(nuevaTarea.value);
+  // Agrega la nueva tarea al array de tareas
+  nuevaTarea.value = '';
+  // Limpia la nueva tarea
+}
+
+</script>
+
+
+<style> </style>
